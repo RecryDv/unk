69a10aa837d270d23fea20e29d28de7c94286b40
RecryDv <166589923+RecryDv@users.noreply.github.com>
2024-05-13 20:31:48 +0300

    Create inj_test.lua

---
+++ inj_test.lua
@@ -0,0 +1,20 @@
+--!client
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+	Name = "wasd club",
+	LoadingTitle = "code:Destroy()",
+	LoadingSubtitle = "by unkn00wn_guy",
+	ConfigurationSaving = {
+		Enabled = true,
+		FolderName = "EWEWEWEWEWEWEWEWEWEW(wasd club ESP)",
+		FileName = "grrrrrrrrrrrrrrrrrrrrrrr(wasd club cfg)"
+	},
+	KeySystem = false, -- Set this to true to use their key system
+	KeySettings = {
+		Title = "Sirius Hub",
+		Subtitle = "Key System",
+		Note = "Join the discord (discord.gg/sirius)",
+		SaveKey = true,
+		Key = "ABCDEF"
+	}
+})
